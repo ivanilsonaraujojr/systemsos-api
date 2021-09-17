221403b55fd10c5b935324b14fd7a2c3e590452a
Ivanilson <ivanilsonaraujojr@gmail.com>
2021-09-16 21:27:01 -0300

    config: Adicionando base do projeto

---
+++ src/main/java/br/com/sossystem/api/SosApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.sossystem.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sossystem/api/SosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sossystem.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
